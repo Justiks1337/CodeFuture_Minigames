f19cc17ddf4c7270c662710b5cbf524974b82d4f
UltraPokemosha <sfygithub@gmail.com>
2024-02-04 15:22:04 +0300

    change front side

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,16 +1,11 @@
 <script setup>
-import GameList from "@/components/Home/GameList.vue";
 import Lheader from "@/components/layouts/Lheader.vue";
 import Lfooter from "@/components/layouts/Lfooter.vue";
-import ErrorPage from "@/components/ErrorPage/ErrorPage.vue";
-import LoadingPage from "@/components/Loading/LoadingPage.vue";
 </script>
 
 <template>
   <Lheader />
-  <GameList />
-  <ErrorPage />
-  <LoadingPage />
+  <RouterView />
   <Lfooter />
 </template>
 
